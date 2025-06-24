cf2fa1b84eddf07972d4f043c0ae699790c887ee
Harry Sharma <harrysharma1066@gmail.com>
2025-06-24 17:08:37 +0100

    feat: add traversal mechanism for crawler to list all a attribute tags href value

--- single-url.go
+++ single-url.go
@@ -3,19 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func crawler(rawURL string) {
+	url, found := strings.CutSuffix(rawURL, "/")
+	if !found {
+		fmt.Println("Error: removing leading /")
+	}
+	fmt.Print(url)
+
 	res, err := http.Get(rawURL)
 	if err != nil {
 		panic(err)
 	}
-	defer res.Body.Close()
 
 	doc, err := html.Parse(res.Body)
-	s := doc.Data
-	fmt.Println(s)
+	if err != nil {
+		panic(err)
+	}
+	traverse_nodes(url, doc)
+}
 
+func traverse_nodes(urlPrefix string, node *html.Node) {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		for _, a := range node.Attr {
+			if a.Key == "href" {
+				fmt.Printf("href: %s%s\n", urlPrefix, a.Val)
+				break
+			}
+		}
+	}
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		traverse_nodes(urlPrefix, c)
+	}
 }
