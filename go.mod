a384ee86b87bbab0847ceda915327061b70fbc2a
Harry Sharma <harrysharma1066@gmail.com>
2025-06-24 18:27:26 +0100

    feat: start writing storage function for crawled data

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.23.0
 
 toolchain go1.23.10
 
-require golang.org/x/net v0.41.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+	golang.org/x/net v0.41.0 // indirect
+)
--- single-url.go
+++ single-url.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
-func crawler(rawURL string) {
+type crawledData struct {
+	url             string
+	textBasedHTMl   string
+	parserBasedHTML *html.Node
+}
+
+func listAllLinks(rawURL string) []string {
 	url, found := strings.CutSuffix(rawURL, "/")
 	if !found {
 		fmt.Println("Error: removing leading /")
 	}
-	fmt.Print(url)
 
 	res, err := http.Get(rawURL)
 	if err != nil {
@@ -24,19 +31,58 @@ func crawler(rawURL string) {
 	if err != nil {
 		panic(err)
 	}
-	traverse_nodes(url, doc)
+
+	return traverse_nodes(url, doc)
+
 }
 
-func traverse_nodes(urlPrefix string, node *html.Node) {
+func traverse_nodes(urlPrefix string, node *html.Node) []string {
+	var links []string
 	if node.Type == html.ElementNode && node.Data == "a" {
 		for _, a := range node.Attr {
 			if a.Key == "href" {
-				fmt.Printf("href: %s%s\n", urlPrefix, a.Val)
-				break
+				if a.Val != "javascript:void(0)" {
+					links = append(links, urlPrefix+a.Val)
+					break
+				}
 			}
 		}
 	}
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		traverse_nodes(urlPrefix, c)
+		childLinks := traverse_nodes(urlPrefix, c)
+		links = append(links, childLinks...)
 	}
+	return links
+}
+
+func store() {
+	const file = "crawledData.db"
+	db, err := sql.Open("sqlite3", file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(db)
+
+}
+
+func crawler(rawURL string) {
+	hrefs := listAllLinks(rawURL)
+	var data = []crawledData{}
+	for i := range hrefs {
+		res, err := http.Get(hrefs[i])
+		if err != nil {
+			panic(err)
+		}
+		htmlBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+		doc, err := html.Parse(res.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		data = append(data, crawledData{url: hrefs[i], textBasedHTMl: string(htmlBody), parserBasedHTML: doc})
+	}
+	store()
 }
